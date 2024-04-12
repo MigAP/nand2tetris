72d205b279ee0155c0c967b680f05521bfdc2388
Miguel Arpa Perozo <miguel.arpa.p@gmail.com>
2024-04-12 21:52:55 +0200

    fill exercice

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,86 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 //// Replace this comment with your code.
+
+// nwords = 8192 (8K words) representing each 16 pixels
+@8192
+D=A
+@nwords
+M=D
+
+// Wait for keyboard measurement
+(KBLOOP)
+// i = 0
+@i
+M=0
+@KBD
+D=M
+// if keypress goto BLACK
+@BLACK
+D;JNE
+// otherwise goto WHITE
+@WHITE
+0;JMP
+
+(BLACK)
+// if i = nwords goto KBLOOP
+@i
+D=M
+@nwords
+D = D - M
+@KBLOOP
+D;JEQ
+// turn black RAM[SCREEN+i] pixel
+// A = SCREEN + i 
+@i
+D=M
+@SCREEN
+A = A + D
+// pixel goes black
+M = -1 
+// i = i + 1
+@i
+M=M+1
+// if keypress goto BLACK otherwise goto WHITE
+@KBD
+D=M
+// if keypress goto BLACK
+@BLACK
+D;JNE
+// otherwise goto KBLOOP
+@KBLOOP
+0;JMP
+
+(WHITE)
+// if i = nwords goto KBLOOP
+@i
+D=M
+@nwords
+D = D - M
+@KBLOOP
+D;JEQ
+// turn white RAM[SCREEN+i] pixel
+// A = SCREEN + i 
+@i
+D=M
+@SCREEN
+A = A + D
+// pixel goes black
+M = 0 
+// i = i + 1
+@i
+M=M+1
+// if keypress goto KBLOOP otherwise goto WHITE
+@KBD
+D=M
+// if keypress goto KBLOOP
+@KBLOOP
+D;JNE
+// otherwise goto WHITE
+@WHITE
+0;JMP
+
+(END)
+@END
+0;JMP
+
