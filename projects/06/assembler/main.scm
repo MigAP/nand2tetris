376dca0bd162f749e9ea54c0c6c0411831a26f7a
Miguel Arpa Perozo <miguel.arpa.p@gmail.com>
2024-05-19 13:31:07 +0200

    successfull tests

--- projects/06/assembler/main.scm
+++ projects/06/assembler/main.scm
@@ -3,12 +3,25 @@
 (import srfi-152) ; string help functions
 (import srfi-48) ; string format help function
 
-(include-relative "./projects/06/assembler/code-module.scm")
-(include-relative "./projects/06/assembler/parser.scm")
+;; (include-relative "./projects/06/assembler/code-module.scm")
+;; (include-relative "./projects/06/assembler/parser.scm")
+(include-relative "./code-module.scm")
+(include-relative "./parser.scm")
 
-(call-with-input-file "projects/06/assembler/prog.asm"
-parser) 
+
+;; (call-with-input-file "projects/06/assembler/prog.asm"
+;; parser) 
 
 (define (assembler file-path)
   (call-with-input-file file-path
     parser))
+
+;; (assembler "projects/06/assembler/prog.asm")
+;; (assembler "./prog.asm")
+;; (newline)
+;; (display "Add program")
+;; (newline)
+;; (assembler "../add/Add.asm")
+;; (assembler "../max/MaxL.asm")
+(assembler "../pong/PongL.asm")
+;; (assembler "../rect/RectL.asm")
